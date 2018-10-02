0c73002f4b65da0f7246aed6128bde5f3ec0b10c
IS Choi <insil321@hotmail.com>
2018-10-02 02:45:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ if katz_deli.length >= 1
     array =[]
     counter= 1
     katz_deli.each do |name|
-      array.push ("#{counter}. #{name}")
+     array.push ("#{counter}. #{name}")
       counter +=1
     end
-    puts "The line is currently:#{nuarray.join(" ")}"
+    puts "The line is currently:#{array.join(" ")}"
    else 
    puts "The line is currently empty."
 end 
