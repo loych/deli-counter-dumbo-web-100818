b375a741b7d1e10ebd2f00104cd7a899089844e4
IS Choi <insil321@hotmail.com>
2018-10-02 02:35:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,8 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+end
+
 
 def now_serving(katz_deli, name)
   if katz_deli.length ==0 
