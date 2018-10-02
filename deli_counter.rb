a0dfaa8223364100e2760370d1761da250b200a5
IS Choi <insil321@hotmail.com>
2018-10-02 02:27:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,8 @@ end
 def now_serving(katz_deli, name)
   if katz_deli.length ==0 
     puts "There is nobody waiting to be served!"
-  else puts 
-  
-  katz_deli.shift(name)
-  puts "currently serving #{name}"
+  else 
+    puts "Currently serving #{katz_deli.shift(name)}"
 end 
 end
   
