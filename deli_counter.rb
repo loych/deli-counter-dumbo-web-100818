13ce2eb00406afe3e3592a3d9927d0fd9b0d63fe
IS Choi <insil321@hotmail.com>
2018-10-02 02:43:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,8 @@ end
 def now_serving(katz_deli, name)
   if katz_deli.length ==0 
     puts "There is nobody waiting to be served!"
+  end
   else 
     puts "Currently serving #{katz_deli.first}"
 end 
+end
