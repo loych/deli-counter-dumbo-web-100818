97c918805dacf5d9c51e84eba1f3f6271f4a150f
IS Choi <insil321@hotmail.com>
2018-10-02 02:37:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,15 +6,14 @@ if katz_deli.length >= 1
       array.push ("#{counter}. #{name}")
       counter +=1
     end
-    puts "The line is currently:#{nuarray.join(" ")}""
-  end
-   else puts "The line is currently empty."
+    puts "The line is currently:#{nuarray.join(" ")}"
+   else 
+   puts "The line is currently empty."
 end 
 
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
 end
 
 
